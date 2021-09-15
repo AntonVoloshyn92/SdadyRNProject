240985d2472a29de3cc46ce988348a2ed47821dd
Anton Voloshyn <antonvoloshyn@Antons-MacBook-Pro.local>
2021-09-15 16:53:30 +0300

    add redux in news screen

--- src/screens/NewsScreen.tsx
+++ src/screens/NewsScreen.tsx
@@ -1,25 +1,21 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import {View, FlatList} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
 import {Articles} from '../interfaces/NewsInterface';
 import NewsCard from '../components/NewsCard';
 import NewsService from '../services/NewsService';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {NewsStackParamList} from '../navigation/navigation.types';
-import {AppState} from '../store/app.types';
+import {connect} from 'react-redux';
+import {RootState} from '../store';
+import {isWhiteThemeSelector} from '../store/app/app.selector';
 
 interface NewsScreenProps {}
 
-const NewsScreen: React.FC<NewsScreenProps> = () => {
+const NewsScreen: React.FC<
+  NewsScreenProps & ReturnType<typeof mapStateToProps>
+> = ({isWhiteTheme}) => {
   const [news, setNews] = useState<Articles[]>([]);
-
-  const mapThema = (state: AppState) => ({
-    isWhiteTheme: state.isWhiteTheme,
-  });
-
-  console.log('====================================');
-  console.log('mapThema', mapThema);
-  console.log('====================================');
 
   const navigation =
     useNavigation<StackNavigationProp<NewsStackParamList, 'NewsScreen'>>();
@@ -31,12 +27,17 @@ const NewsScreen: React.FC<NewsScreenProps> = () => {
 
   const query = 'https://newsapi.org/v2/top-headlines';
 
+  console.log('====================================');
+  console.log(isWhiteTheme);
+  console.log('====================================');
+
   useEffect(() => {
     fetchNewsCallback(query);
   }, [fetchNewsCallback, query]);
 
   return (
-    <View>
+    <View
+      style={[styles.workSpace, isWhiteTheme ? styles.main : styles.mainDark]}>
       <FlatList
         data={news}
         keyExtractor={(item, index) => 'key' + index}
@@ -44,7 +45,7 @@ const NewsScreen: React.FC<NewsScreenProps> = () => {
           return (
             <NewsCard
               article={item}
-              onClick={url => {
+              onClick={() => {
                 navigation.navigate('NewsDetailsScreen', {
                   article: item,
                 });
@@ -57,4 +58,24 @@ const NewsScreen: React.FC<NewsScreenProps> = () => {
   );
 };
 
-export default NewsScreen;
+const mapStateToProps = (state: RootState) => ({
+  isWhiteTheme: isWhiteThemeSelector(state),
+});
+
+const styles = StyleSheet.create({
+  workSpace: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  mainDark: {
+    flex: 1,
+    backgroundColor: '#6e798a',
+  },
+});
+
+export default connect(mapStateToProps)(NewsScreen);
