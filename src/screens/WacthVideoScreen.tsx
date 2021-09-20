f42b303b1cc66a56a1c8bef64e46c7673fe25409
Anton Voloshyn <antonvoloshyn@Antons-MacBook-Pro.local>
2021-09-20 11:55:48 +0300

    add video

--- src/screens/WacthVideoScreen.tsx
+++ src/screens/WacthVideoScreen.tsx
@@ -1,132 +1,85 @@
 import {RouteProp, useRoute} from '@react-navigation/native';
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import Video from 'react-native-video';
+import React, {useRef, useState} from 'react';
+import {StyleSheet, View, Alert} from 'react-native';
 import {VideStackParamList} from '../navigation/navigation.types';
+import YoutubePlayer from 'react-native-youtube-iframe';
+import {Icon} from 'react-native-elements';
 
-// const WacthVideoScreen: React.FC<any> = () => {
-//   const route = useRoute<RouteProp<VideStackParamList, 'WacthVideoScreen'>>();
-//   const {item} = route.params;
+const WacthVideoScreen: React.FC<any> = () => {
+  const route = useRoute<RouteProp<VideStackParamList, 'WacthVideoScreen'>>();
+  const {item} = route.params;
 
-//   console.log('====================================');
-//   console.log(item.id.videoId);
-//   console.log(`https://www.youtube.com/watch?v=${item.id.videoId}`);
-//   console.log('====================================');
+  const [playing, setPlaying] = useState(false);
+  const [isMute, setMute] = useState(false);
+  const controlRef = useRef();
 
-//   return (
-//     <Video
-//       source={{uri: `https://www.youtube.com/watch?v=${item.id.videoId}`}} // Can be a URL or a local file.
-//       style={styles.backgroundVideo}
-//     />
-//   );
-// };
-
-const WacthVideoScreen = () => {
-  const videoPlayer = useRef(null);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [duration, setDuration] = useState(0);
-  const [isFullScreen, setIsFullScreen] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [paused, setPaused] = useState(false);
-  const [playerState, setPlayerState] = useState(PLAYER_STATES.PLAYING);
-  const [screenType, setScreenType] = useState('content');
-
-  const onSeek = seek => {
-    //Handler for change in seekbar
-    videoPlayer.current.seek(seek);
-  };
-
-  const onPaused = playerState => {
-    //Handler for Video Pause
-    setPaused(!paused);
-    setPlayerState(playerState);
-  };
-
-  const onReplay = () => {
-    //Handler for Replay
-    setPlayerState(PLAYER_STATES.PLAYING);
-    videoPlayer.current.seek(0);
-  };
-
-  const onProgress = data => {
-    // Video Player will progress continue even if it ends
-    if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
-      setCurrentTime(data.currentTime);
+  const onStateChange = (state: any) => {
+    if (state === 'ended') {
+      setPlaying(false);
+      Alert.alert('video has finished playing!');
+    }
+    if (state !== 'playing') {
+      setPlaying(false);
     }
   };
 
-  const onLoad = data => {
-    setDuration(data.duration);
-    setIsLoading(false);
+  const togglePlaying = () => {
+    setPlaying(prev => !prev);
   };
-
-  const onLoadStart = data => setIsLoading(true);
-
-  const onEnd = () => setPlayerState(PLAYER_STATES.ENDED);
-
-  const onError = () => alert('Oh! ', error);
-
-  const exitFullScreen = () => {
-    alert('Exit full screen');
+  const seekBackAndForth = (control: any) => {
+    console.log('currentTime');
+    controlRef.current?.getCurrentTime().then(currentTime => {
+      control === 'forward'
+        ? controlRef.current?.seekTo(currentTime + 15, true)
+        : controlRef.current?.seekTo(currentTime - 15, true);
+    });
   };
-
-  const enterFullScreen = () => {};
-
-  const onFullScreen = () => {
-    setIsFullScreen(isFullScreen);
-    if (screenType == 'content') setScreenType('cover');
-    else setScreenType('content');
-  };
-
-  const renderToolbar = () => (
-    <View>
-      <Text style={styles.toolbar}> toolbar </Text>
-    </View>
+  const muteVideo = () => setMute(!isMute);
+  const ControlIcon = ({name, onPress}: any) => (
+    <Icon onPress={onPress} name={name} size={40} color="#fff" />
   );
 
-  const onSeeking = currentTime => setCurrentTime(currentTime);
-
   return (
-    <View style={{flex: 1}}>
-      <Video
-        onEnd={onEnd}
-        onLoad={onLoad}
-        onLoadStart={onLoadStart}
-        onProgress={onProgress}
-        paused={paused}
-        ref={videoPlayer}
-        resizeMode={screenType}
-        onFullScreen={isFullScreen}
-        source={{
-          uri: 'https://assets.mixkit.co/videos/download/mixkit-countryside-meadow-4075.mp4',
-        }}
-        style={styles.mediaPlayer}
-        volume={10}
+    <View style={styles.container}>
+      <YoutubePlayer
+        height={300}
+        ref={controlRef}
+        play={playing}
+        mute={isMute}
+        videoId={item.id.videoId}
+        onChangeState={onStateChange}
       />
-      <MediaControls
-        duration={duration}
-        isLoading={isLoading}
-        mainColor="#333"
-        onFullScreen={onFullScreen}
-        onPaused={onPaused}
-        onReplay={onReplay}
-        onSeek={onSeek}
-        onSeeking={onSeeking}
-        playerState={playerState}
-        progress={currentTime}
-        toolbar={renderToolbar()}
-      />
+      <View style={styles.controlContainer}>
+        <ControlIcon
+          onPress={() => seekBackAndForth('rewind')}
+          name="skip-previous"
+        />
+        <ControlIcon
+          onPress={togglePlaying}
+          name={playing ? 'pause' : 'play-arrow'}
+        />
+        <ControlIcon
+          onPress={() => seekBackAndForth('forward')}
+          name="skip-next"
+        />
+        <ControlIcon
+          onPress={muteVideo}
+          name={isMute ? 'volume-up' : 'volume-off'}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+  container: {
+    backgroundColor: 'lightgrey',
+    flex: 1,
+  },
+  controlContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
 
