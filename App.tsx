2f2a184eabd62df6fb2098e8fb2b25c308e4b26f
Anton Voloshyn <antonvoloshyn@Antons-MacBook-Pro.local>
2021-09-20 17:44:51 +0300

    fix app.tsx

--- App.tsx
+++ App.tsx
@@ -4,23 +4,17 @@ import BottomNavigation from './src/navigation/BottomNavigation';
 import './src/constants/translations/i18n';
 import {Provider} from 'react-redux';
 import {store} from './src/store';
-import remoteConfig, {firebase} from '@react-native-firebase/remote-config';
-import {setIsWhiteThemaThunk} from './src/store/app/app.thunks';
+import remoteConfig from '@react-native-firebase/remote-config';
 
 const App = () => {
   remoteConfig()
-    .setDefaults({
-      isWhiteThema: true,
-    })
-    .then(() => remoteConfig().fetchAndActivate())
-    .then(fetchedRemotely => {
-      const isWhite: boolean = remoteConfig().getBoolean('isWhiteThema');
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-      console.log(isWhite);
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    .fetch(0)
+    .then(() => {
+      return remoteConfig().activate();
     });
 
-  // dispatch(setIsWhiteThemaThunk(isWhiteThema));
+  const isWhite: boolean = remoteConfig().getBoolean('isWhiteThema');
+  console.log(isWhite);
 
   return (
     <Provider store={store}>
